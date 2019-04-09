36dfa345f18d43ac1229219f505dca1aed09c63d
MarvinChung <marvin852316497@gmail.com>
2019-04-09 23:30:47 +0800

    remove nltk add split and re

--- hw2/hw2-1/make_dictionary.py
+++ hw2/hw2-1/make_dictionary.py
@@ -1,7 +1,7 @@
 import json
-import nltk
-from nltk import word_tokenize
-
+#import nltk
+#from nltk import word_tokenize
+import re
 idfilename = 'training_data/id.txt'
 datadirname = 'training_data/feat/'
 labelfilename = 'training_label.json'
@@ -15,18 +15,21 @@ EOS = "<eos>"
 PAD = "<pad>"
 UNK = "<unk>"
 
-DIC_word_index[BOS] = 0
-DIC_word_index[EOS] = 1
-DIC_word_index[PAD] = 2
-DIC_word_index[UNK] = 3
+#start from 1, because padding will pad zero into it.
+
+DIC_word_index[BOS] = 1
+DIC_word_index[EOS] = 2
+DIC_word_index[PAD] = 3
+DIC_word_index[UNK] = 4
 
 #======Read json file, tokenize and add it to DIC
-index = 4
+index = 5
 rawlabels = json.load(open(labelfilename, 'r'))
 for data in rawlabels:
     for caption in data['caption']:
-        caption = caption.lower()
-        for word in word_tokenize(caption):
+        caption = re.sub(r'[^\w\s\<\>\-]','',caption)
+        tokens = caption.lower().split()
+        for word in tokens:
             if word not in DIC_word_index:
                 DIC_word_index[word] = index
                 index += 1

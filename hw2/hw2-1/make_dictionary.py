cf809780d6bdcb54c560600832b5fce087766f8d
KaiWei <b05901091@ntu.edu.tw>
2019-04-12 20:48:18 +0800

    add dictionary(frequency of a word bigger than a threshold)

--- hw2/hw2-1/make_dictionary.py
+++ hw2/hw2-1/make_dictionary.py
@@ -10,9 +10,10 @@ labelfilename = 'training_label.json'
 DIC_index_word = {}
 DIC_word_index = {}
 
+
+PAD = "<pad>"
 BOS = "<bos>"
 EOS = "<eos>"
-PAD = "<pad>"
 UNK = "<unk>"
 
 DIC_word_index[PAD] = 0
@@ -21,21 +22,49 @@ DIC_word_index[EOS] = 2
 DIC_word_index[UNK] = 3
 
 #======Read json file, tokenize and add it to DIC
-index = 4
-rawlabels = json.load(open(labelfilename, 'r'))
-for data in rawlabels:
-    for caption in data['caption']:
-        tokens = word_tokenize(caption.lower())
-        for word in tokens:
-            if word not in DIC_word_index:
-                DIC_word_index[word] = index
-                index += 1
-                
-#====== key, value inversion
-DIC_index_word = dict((v,k) for k,v in DIC_word_index.items())
-
-with open("DIC_word_index.json", 'w') as f:
-    json.dump(DIC_word_index, f)
+def make_naive_dic():
+    index = 4
+    rawlabels = json.load(open(labelfilename, 'r'))
+    for data in rawlabels:
+        for caption in data['caption']:
+            tokens = word_tokenize(caption.lower())
+            for word in tokens:         
+                if word not in DIC_word_index:
+                    DIC_word_index[word] = index
+                    index += 1
+def make_freq_dic():    
+    word_freq = {}
+    rawlabels = json.load(open(labelfilename, 'r'))
+    for data in rawlabels:
+        for caption in data['caption']:
+            tokens = word_tokenize(caption.lower())
+            for word in tokens:
+                if word not in word_freq:
+                    word_freq[word] = 1
+                elif word in word_freq:
+                    word_freq[word] += 1
     
-with open("DIC_index_word.json", 'w') as f:
-    json.dump(DIC_index_word, f)
+    index = 4 
+    for word in word_freq:
+        if word_freq[word] >= 4:
+            DIC_word_index[word] = index
+            index +=1
+                
+    
+if __name__ == '__main__':
+    #====== choose one 
+    #make_naive_dic()
+    make_freq_dic()
+    
+    #====== key, value inversion
+    DIC_index_word = dict((v,k) for k,v in DIC_word_index.items())
+    
+    #====== write to json file
+    with open("DIC_word_index.json", 'w') as f:
+        json.dump(DIC_word_index, f)
+    
+    with open("DIC_index_word.json", 'w') as f:
+        json.dump(DIC_index_word, f)
+
+
+
